329bc46052dcf22ec422b290349b95c270ac70ec
Samarth Shinde <samarth.s@videosdk.live>
2026-04-06 16:11:48 +0530

    reverted

--- src/meeting/components/AudioPlayer.js
+++ src/meeting/components/AudioPlayer.js
@@ -9,16 +9,7 @@ export const ParticipantAudioPlayer = ({ participantId }) => {
       const mediaStream = new MediaStream();
       mediaStream.addTrack(micStream.track);
       audioRef.current.srcObject = mediaStream;
-      audioRef.current.play().catch((err) => {
-        if (
-          err.message ===
-          "play() failed because the user didn't interact with the document first. https://goo.gl/xX8pDD"
-        ) {
-          console.log("audio " + err.message);
-        } else {
-          console.log("audio catch", err);
-        }
-      });
+      audioRef.current.play();
     }
   }, [micStream]);
   return (
